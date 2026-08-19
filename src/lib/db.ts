ac8f0670780dd1234d1467394cc477d34c7aa35e
RHL Kumar <rhlkumar135@gmail.com>
2026-08-19 03:43:38 +0000

    Add PrismaPg adapter for PostgreSQL

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,13 +1,22 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright (C) 2026 Shogo Technologies, Inc.
+import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '../generated/prisma/client'
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
+function createPrismaClient() {
+  const url = process.env.DATABASE_URL
+  if (url) {
+    const adapter = new PrismaPg({ connectionString: url })
+    return new PrismaClient({ adapter })
+  }
+  return new PrismaClient({})
+}
+
 export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({})
+  globalForPrisma.prisma ?? createPrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
